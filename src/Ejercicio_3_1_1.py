05b7326a736ba8ca5dbe1f26bac40b70d14ccc2d
dbenfon341 <dbenfon341@g.educaand.es>
2023-11-13 19:20:18 +0100

    .

--- src/Ejercicio_3_1_1.py
+++ src/Ejercicio_3_1_1.py
@@ -1,6 +1,7 @@
 # Ejercicio 3.1.1
 # Escribir un programa que almacene las asignaturas de un curso (por ejemplo Matemáticas, Física, Química, Historia y Lengua) en una lista y la muestre por pantalla.
 
+
 def asignaturas():
     materias = ["Matemáticas", "Física", "Química", "Historia", "Lengua"]
     for materia in materias:
