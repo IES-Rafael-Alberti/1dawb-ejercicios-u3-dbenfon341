047c4470b5403d56ea203ff7484b4d19da6a9565
dbenfon341 <dbenfon341@g.educaand.es>
2023-11-15 17:13:21 +0100

    Ejercicio 6 y 7.

--- src/Ejercicio_3_1_6.py
+++ src/Ejercicio_3_1_6.py
@@ -4,7 +4,30 @@
 # Al final el programa debe mostrar por pantalla las asignaturas que el usuario tiene que repetir.
 
 
+def asignaturas():
+    materias = ["Matemáticas", "Física", "Química", "Historia", "Lengua"]
+    return materias
+
+
+def dame_notas(materias):
+    notas = []
+    for materia in materias:
+        nota = float(input(f"Ingrese la nota de {materia}: "))
+        notas.append(nota)
+    return notas
+
+def mostrar_notas(materias, notas):
+    for i in range(len(notas)):
+        if notas[i] >= 5:
+            print(f"En {materias[i]} has sacado {notas[i]}.")
+        else:
+            print(f"Debe repetir {[materias[i]]}.")
+
+
 def main():
+    materias = asignaturas()
+    notas = dame_notas(materias)
+    mostrar_notas(materias, notas)
 
 
 if __name__ == "__main__":
