90d6c14c04e3334befe80dcbd704b03fa9546972
dbenfon341 <dbenfon341@g.educaand.es>
2023-11-22 17:29:56 +0100

    Ejercicios.

--- src/Ejercicio_3_2_8.py
+++ src/Ejercicio_3_2_8.py
@@ -4,9 +4,23 @@
 # El programa debe crear un diccionario con las palabras y sus traducciones. 
 # Después pedirá una frase en español y utilizará el diccionario para traducirla palabra a palabra. Si una palabra no está en el diccionario debe dejarla sin traducir.
 
+def pedir_palabra_traducida():
+
+
+def traducir_frase(traductor, frase_espanol):
+
 
 def main():
 
 
 if __name__ == "__main__":
     main()
+
+
+
+
+dinero:money, cabeza:head, amor:love
+
+Yo tengo dinero, cabeza, pa hacer el amor.
+
+Yo tengo money, head, pa hacer el love.
--- src/Ejercicio_3_3_2.py
+++ src/Ejercicio_3_3_2.py
@@ -9,6 +9,30 @@
 
 
 def main():
+    nombres_primaria = set()
+    nombres_secundaria = set()
+    while True:
+        nombre_primaria = input("Introduce el nombre de un alumno de primaria (introduce x para salir): ").title()
+        nombres_primaria.add(nombre_primaria)
+        if nombre_primaria == "X":
+            break
+    while True:
+        nombre_secundaria = input("Introduce el nombre de un alumno de secundaria (introduce x para salir): ").title()
+        nombres_secundaria.add(nombre_secundaria)
+        if nombre_secundaria == "X":
+             break
+    nombres_secundaria.remove("X")
+    nombres_primaria.remove("X")
+
+    nombres_unidos = nombres_primaria | nombres_secundaria
+    nombres_repetidos = nombres_primaria & nombres_secundaria
+    nombres_no_repetidos_secundaria = nombres_primaria - nombres_secundaria
+    nombres_no_repetidos_primaria = nombres_secundaria - nombres_primaria
+
+    print (f"Los nombres unidos son: {nombres_unidos}")
+    print (f"Los nombres que se repiten entre los alumnos de primaria y secundaria son: {nombres_repetidos}")
+    print (f"Los nombres que no se repiten en los de nivel secundaria son: {nombres_no_repetidos_secundaria}")
+    print (f"Los nombres que no se repiten en los de nivel primaria son: {nombres_no_repetidos_primaria}")
 
 
 if __name__ == "__main__":
